c149e4cbacc0164473df82a861c272338d528323
iAjiann <aldwinsantos7@gmail.com>
2026-04-23 19:21:03 +0800

    Draft

---
+++ SignifyUI/HomePage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace SignifyUI
+{
+    public partial class HomePage : Page
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void Freehand_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Freehand clicked");
+        }
+
+        private void Learn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService?.Navigate(new LearnPage());
+        }
+
+        private void Sentence_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Sentence Builder clicked");
+        }
+
+        private void Settings_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Settings clicked");
+        }
+    }
+}
---
+++ SignifyUI/Pages/LearnPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace SignifyUI
+{
+    public partial class LearnPage : Page
+    {
+        public LearnPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Home_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService?.Navigate(new HomePage());
+        }
+
+        private void TopSettings_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Top settings clicked");
+        }
+
+        private void Profile_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Profile clicked");
+        }
+
+        private void LetterA_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Letter A clicked");
+        }
+    }
+}
